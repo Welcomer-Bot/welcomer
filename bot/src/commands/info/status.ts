e263dc37a5a77689b6e763f61bb114479d53c43e
Clément <idleimperiator@gmail.com>
2024-11-13 22:51:11 +0100

    remove unused code in status command

--- bot/src/commands/info/status.ts
+++ bot/src/commands/info/status.ts
@@ -44,52 +44,6 @@ export default class StatusCommand implements CommandType {
             return sendInteractionMessage(interaction, { content: "The client is not ready yet" })
         }
 
-
-        const res = await client.cluster.broadcastEval((c) => {
-          return {
-            clusterId: c.cluster.id,
-            shardIds: [...c.cluster.shardList],
-            totalGuilds: c.guilds.cache.size,
-            totalMembers: c.guilds.cache
-              .map((g) => g.memberCount)
-              .reduce((a, b) => a + b, 0),
-            ping: c.ws.ping,
-            uptime: c.uptime,
-            memoryUsage: Object.fromEntries(
-              Object.entries(process.memoryUsage()).map((d) => {
-                d[1] = Math.floor((d[1] / 1024 / 1024) * 100) / 100; // format to MB
-                return d;
-              })
-            ), // { rss: memInMB, heapUsed: memInMb, ... }
-            // if you need specific data of all guilds, you don't really need totalGuilds
-            // cause then you can do allGuildsData.length
-            allGuildsData: c.guilds.cache.map((guild) => {
-              return {
-                id: guild.id,
-                name: guild.name,
-                ownerId: guild.ownerId,
-                memberCount: guild.memberCount,
-                channels: guild.channels.cache.map((c) => {
-                  return { id: c.id, name: c.name }; // It's important not to return the entire CLASS
-                }),
-              };
-            }),
-            perShardData: [...c.cluster.shardList].map((shardId) => {
-              return {
-                shardId: shardId,
-                ping: c.ws.shards.get(shardId)?.ping,
-                uptime: c.uptime,
-                guilds: c.guilds.cache.filter((x) => x.shardId === shardId)
-                  .size,
-                members: c.guilds.cache
-                  .filter((x) => x.shardId === shardId)
-                  .map((g) => g.memberCount)
-                  .reduce((a, b) => a + b, 0),
-              };
-            }),
-          };
-        });
-
              let ram;
              let membersize;
              let guildsize;
