0e6309cff9635ebed296ebe0073467368f61bcdb
Clément <idleimperiator@gmail.com>
2024-12-27 16:50:17 +0100

    remove unused console log

--- bot/src/events/guild/guildMemberRemove.ts
+++ bot/src/events/guild/guildMemberRemove.ts
@@ -2,9 +2,9 @@ import { GuildMember, InteractionResponse, Message } from "discord.js";
 import WelcomerClient from "../../structure/WelcomerClient";
 import { createOrUpdateGuild } from "../../utils/createGuild";
 import { addMemberGoodbye, getGuild } from "../../utils/getGuild";
-import { goodbyeCard } from './../../utils/welcomeCard';
+import { goodbyeCard } from "./../../utils/welcomeCard";
 
-import { error } from '../../utils/logger';
+import { error } from "../../utils/logger";
 import { EventType } from "./../../types/index";
 
 export default class GuildMemberRemove implements EventType {
@@ -18,15 +18,13 @@ export default class GuildMemberRemove implements EventType {
       var guilds = await getGuild(guild.id);
       if (guilds) {
         goodbyeCard(member, guild, guilds, client);
-        console.log(guilds);
         if (!guilds.goodbyeer.enabled) return;
         addMemberGoodbye(guild);
       } else {
         await createOrUpdateGuild(member.guild);
       }
-    } catch (err:Error | any) {
+    } catch (err: Error | any) {
       error(err);
     }
   }
 }
-
